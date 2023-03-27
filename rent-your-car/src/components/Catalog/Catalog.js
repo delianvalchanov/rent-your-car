71abdcde1643d9e7e8cacb1a64dd197ec0db3dc3
dValchanovGit <delianvalchanov@gmail.com>
2023-03-27 18:01:17 +0300

    Adding Softuni dev server folder to project

--- rent-your-car/src/components/Catalog/Catalog.js
+++ rent-your-car/src/components/Catalog/Catalog.js
@@ -3,12 +3,12 @@ import { CarArticle } from "./CarArticle"
 import CatalogCSS from "./assets/Catalog.module.css"
 
 export const Catalog = ({
-    cars
+    articles
 }) => {
     return (
         <>
             <div className={`row ${CatalogCSS.wrapper}`}>
-                {cars.map(x => <div className={`col-lg-4 ${CatalogCSS.article}`}> <CarArticle key={x._id} {...x} /> </div>)}
+                {articles.map(x => <div className={`col-lg-4 ${CatalogCSS.article}`}> <CarArticle key={x._id} {...x} /> </div>)}
             </div>
         </>
     )
