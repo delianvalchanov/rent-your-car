8a3ec19040c7e5346ced7a4c1892dd146f44547e
dValchanovGit <delianvalchanov@gmail.com>
2023-09-11 21:53:57 +0300

    Separating logic about form validation
    Separating form validation schema from useControlledForm hook

--- rent-your-car/src/hooks/useControlledForm.js
+++ rent-your-car/src/hooks/useControlledForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FormValidation } from "../validation/FormValidation";
 
 export const useForm = (initialValues, onSubmitHandler) => {
     const [values, setValues] = useState(initialValues);
@@ -14,33 +15,8 @@ export const useForm = (initialValues, onSubmitHandler) => {
 
         onSubmitHandler(values);
         setValues(initialValues);
-        setErrors(validate(values))
+        setErrors(FormValidation(values))
         setIsSubmit(true);
-    }
-
-    const validate = (values) => {
-        const err = {}
-        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
-        if (!values.username) {
-            err.username = 'Username is required!'
-        }
-        if (!values.email) {
-            err.email = 'Email is required!'
-        } else if (!regex.test(values.email)) {
-            err.email = 'This is not valid email format!'
-        }
-        if (!values.password) {
-            err.password = 'Password is required!'
-        } else if (!values.password.length > 5) {
-            err.password = 'Password minimum length is 5 characters!'
-        } else if (!values.password.length < 15) {
-            err.password = 'Password maximum length is 15 characters!'
-        }
-        if (!values.confirmPassword) {
-            err.confirmPassword = 'Confirm password is required!'
-        }
-
-        return err;
     }
 
     const changeValues = (newValues) => {
