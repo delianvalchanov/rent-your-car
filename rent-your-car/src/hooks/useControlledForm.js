71dde17613e10f607cdb2190cb1968c9281a10fa
dValchanovGit <delianvalchanov@gmail.com>
2023-10-09 14:28:18 +0300

    Revert "Separating logic about form validation"
    This reverts commit 8a3ec19040c7e5346ced7a4c1892dd146f44547e.

--- rent-your-car/src/hooks/useControlledForm.js
+++ rent-your-car/src/hooks/useControlledForm.js
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import { FormValidation } from "../validation/FormValidation";
 
 export const useForm = (initialValues, onSubmitHandler) => {
     const [values, setValues] = useState(initialValues);
@@ -15,8 +14,33 @@ export const useForm = (initialValues, onSubmitHandler) => {
 
         onSubmitHandler(values);
         setValues(initialValues);
-        setErrors(FormValidation(values))
+        setErrors(validate(values))
         setIsSubmit(true);
+    }
+
+    const validate = (values) => {
+        const err = {}
+        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+        if (!values.username) {
+            err.username = 'Username is required!'
+        }
+        if (!values.email) {
+            err.email = 'Email is required!'
+        } else if (!regex.test(values.email)) {
+            err.email = 'This is not valid email format!'
+        }
+        if (!values.password) {
+            err.password = 'Password is required!'
+        } else if (!values.password.length > 5) {
+            err.password = 'Password minimum length is 5 characters!'
+        } else if (!values.password.length < 15) {
+            err.password = 'Password maximum length is 15 characters!'
+        }
+        if (!values.confirmPassword) {
+            err.confirmPassword = 'Confirm password is required!'
+        }
+
+        return err;
     }
 
     const changeValues = (newValues) => {
